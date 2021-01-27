1905e43741d71e0a947fb3236009c956c0698a3f
MaryWenzel <wenzel.mary1993@gmail.com>
2021-01-27 14:02:32 -0600

    finished assignment

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { Container, Paper, Chip } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  card: {
+    height: 200,
+    padding: 40,
+  },
+  container: {
+    marginTop: 50,
+  },
+}));
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const id = parseInt(props.match.params.id);
+  const thisCar = cars.filter((car) => car.id === id);
+  const classes = useStyles();
 
-export default Car
+  return (
+    <Container className={classes.container} maxWidth="sm">
+      <Paper className={classes.card} elevation={3}>
+        <h2>{thisCar[0].Name}</h2>
+        <div className={classes.root}>
+          <Chip label={`id: ${thisCar[0].id}`}></Chip>
+          <Chip label={`Name: ${thisCar[0].Name}`}></Chip>
+          <Chip
+            label={`Miles Per Gallon: ${thisCar[0].Miles_per_Gallon}`}
+          ></Chip>
+          <Chip label={`Cylinders: ${thisCar[0].Cylinders}`}></Chip>
+          <Chip label={`Displacement: ${thisCar[0].Displacement}`}></Chip>
+          <Chip label={`Horsepower: ${thisCar[0].Horsepower}`}></Chip>
+          <Chip
+            label={`Weight (in pounds): ${thisCar[0].Weight_in_lbs}`}
+          ></Chip>
+          <Chip label={`Acceleration: ${thisCar[0].Acceleration}`}></Chip>
+          <Chip label={`Year: ${thisCar[0].Year}`}></Chip>
+          <Chip label={`Origin: ${thisCar[0].Origin}`}></Chip>
+        </div>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Car;
